5861547bf5e201d53aa357380bbb843b38531a0f
José Leyva <jrleyva41096@gmail.com>
2022-12-02 20:51:14 -0500

    added product model code, and deleted one unnecessary line of code in category

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,36 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    productName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: DECIMAL,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: NUMERIC
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      references: categoryId
+    }
   },
   {
     sequelize,
