84ce7b260d46432e12cddfc65931612f775ba593
José Leyva <jrleyva41096@gmail.com>
2022-12-02 20:35:43 -0500

    added catergory model code and began coding routes for category

--- models/Category.js
+++ models/Category.js
@@ -1,3 +1,4 @@
+const { INTEGER } = require('sequelize');
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
@@ -6,7 +7,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    categoryName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,15 +6,29 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where:{
+      id: req.params.id
+    },
+    include:[Product]
+  }).then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    
+  })
 });
 
 router.put('/:id', (req, res) => {
