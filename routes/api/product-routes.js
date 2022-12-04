8fc5c72485a0bf187e9f02a79f984e4054681e4f
José Leyva <jrleyva41096@gmail.com>
2022-12-03 19:17:07 -0500

    added find one route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,21 +4,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  try {
-    const productData = await Product.findAll({ include: Category, Tag });
-    res.status(200).json(productData)
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  Product.findAll({
+    include: Category, Tag
+  });
+  res.status(200).json(productData)
+}).then((err) => {
+  res.status(500).json(err);
 });
+
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Category, Tag]
+  }).then((ctgData) => {
+    res.json(ctgData);
+  });
 });
 
 // create new product
